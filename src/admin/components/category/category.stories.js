00808360bf1e39b441d6f76e98c865d42cd529f0
Egor Ivanov <egerix@gmail.com>
2020-11-16 16:12:02 +0300

    страница скилов в админке

---
+++ src/admin/components/category/category.stories.js
@@ -0,0 +1,34 @@
+import category from "./category";
+import { action } from "@storybook/addon-actions";
+
+const methods = {
+    onRemove: action("onRemove")
+}
+
+export default {
+    title: "category",
+    components: {category}
+}
+
+const skills = [
+    {id: 0, title: "CSS", percent: 80},
+    {id: 1, title: "HTML", percent: 50},
+]
+
+export const defaultView = () => ({
+    components: {category},
+    data() {
+        return {
+            title: "Frontend",
+            skills
+        }
+    },
+    template: `
+      <category
+          :title="title"
+          :skills="skills"
+          @remove="onRemove"
+      />
+    `,
+    methods,
+})
---
+++ src/admin/components/user/user.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="user-component">
+    <avatar :size="'2.7'" :src="userPic"/>
+    <div class="username">Егор Иванов</div>
+  </div>
+</template>
+
+<script>
+import avatar from "../avatar";
+
+export default {
+  components: {
+    avatar
+  },
+  computed: {
+    userPic(){
+      return require("../../../images/content/user.jpg").default
+    }
+  }
+}
+
+</script>
+
+<style lang="postcss">
+  .user-component {
+    display: flex;
+    font-size: 18px;
+    font-weight: 600;
+    align-items: center;
+  }
+
+  .username {
+    margin-left: 10px;
+    color: #ffffff;
+  }
+</style>
